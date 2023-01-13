3af4b593d0e66d1860ade69d85e5a1c087202830
Valeria Naldi <32465552+vanaldi@users.noreply.github.com>
2023-01-13 06:25:45 -0800

    Update TestLogGenerator.ps1

--- TestLogGenerator.ps1
+++ TestLogGenerator.ps1
@@ -4,7 +4,7 @@
 #information needed to authenticate to AAD and obtain a bearer token
 $tenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47"; #Tenant ID the data collection endpoint resides in
 $appId = "0bfd3e16-224e-4ec3-9b05-10aa61306118"; #Application ID created and granted permissions
-$appSecret = "H6s8Q~whvXfaQrvAKbzYmr_v-GJNGohOXUcxqb_x"; #Secret created for the application
+$appS = ""; #Secret created for the application
 
 #information needed to send data to the DCR endpoint
 $dcrImmutableId = "dcr-b445c63751704f2ea541d8f517acbade"; #the immutableId property of the DCR object
@@ -14,7 +14,7 @@ $dceEndpoint = "https://assetusageendpoint-3j0y.westus2-1.ingest.monitor.azure.c
 ### Step 1: Obtain a bearer token used later to authenticate against the DCE.
 ##################
 $scope= [System.Web.HttpUtility]::UrlEncode("https://monitor.azure.com//.default")   
-$body = "client_id=$appId&scope=$scope&client_secret=$appSecret&grant_type=client_credentials";
+$body = "client_id=$appId&scope=$scope&client_secret=$appS&grant_type=client_credentials";
 $headers = @{"Content-Type"="application/x-www-form-urlencoded"};
 $uri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
 

5fd96c6348601e45cccb228a45b8de4d577a5723
Valeria Naldi <32465552+vanaldi@users.noreply.github.com>
2023-01-30 09:53:57 -0800

    Add files via upload

---
+++ AssetUsageLogs.ps1
@@ -0,0 +1,23 @@
+Add-Type -AssemblyName System.Web;
+$scope= [System.Web.HttpUtility]::UrlEncode("https://monitor.azure.com//.default")   
+$body = "client_id=$appId&scope=$scope&client_secret=$appSecret&grant_type=client_credentials";
+$headers = @{"Content-Type"="application/x-www-form-urlencoded"};
+$uri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
+$bearerToken = (Invoke-RestMethod -Uri $uri -Method "Post" -Body $body -Headers $headers).access_token
+$currentTime = Get-Date ([datetime]::UtcNow) -Format O
+$staticData = @"
+[
+{
+    "Time": "$currentTime",
+    "ResourceGroup": "TestRG",
+    "ResourceName": "$resourceName",
+    "ResourceType": "resourceType"  
+}
+]
+"@;
+
+$body = $staticData;
+echo $body;
+$headers = @{"Authorization"="Bearer $bearerToken";"Content-Type"="application/json"};
+$uri = "$dceEndpoint/dataCollectionRules/$dcrImmutableId/streams/Custom-AssetUsage_CL?api-version=2021-11-01-preview"
+$uploadResponse = Invoke-RestMethod -Uri $uri -Method "Post" -Body $body -Headers $headers
